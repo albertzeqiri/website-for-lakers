156a0306d648e0ecb55ad11940f9e0b06e727e11
Albert Zeqiri <albertzeqiri00@gmail.com>
2024-02-08 17:16:33 +0100

    Add files via upload

---
+++ Website for Lakers/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var voteButton = document.querySelector("#vote-button");
+
+  voteButton.addEventListener("click", function () {
+    alert("Thank you for voting!");
+  });
+});
