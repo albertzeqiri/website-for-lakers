87e9a0b303acd96b863fdd6e7797b395c428db71
Albert Zeqiri <albertzeqiri00@gmail.com>
2024-02-08 17:20:51 +0100

    Delete Website for Lakers directory

--- Website for Lakers/scripts/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var voteButton = document.querySelector("#vote-button");
-
-  voteButton.addEventListener("click", function () {
-    alert("Thank you for voting!");
-  });
-});
